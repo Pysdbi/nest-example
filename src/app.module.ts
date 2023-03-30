91f2fcb9ad49fe21e6c12410597cf2cc10a500d8
aleksey.topchy <Aleksey.Topchy@x5.ru>
2023-03-30 07:37:48 +0300

    Database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as PgConnParser from 'pg-connection-string';
+import { AppController } from './app.controller';
 import { Company, CompanyModule } from './domains/company';
+import * as fs from 'fs';
+
+const databaseUrl: string = process.env.DB_URL;
+const conn = PgConnParser.parse(databaseUrl);
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '127.0.0.1',
-      username: 'root',
-      password: 'root',
-      database: 'main-db',
+      host: conn.host,
+      port: Number(conn.port),
+      database: conn.database,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       applicationName: 'nest-app',
       entities: [Company],
       synchronize: true,
       logging: true,
+      ssl: {
+        ca:
+          process.env.CA_CERT ||
+          fs.readFileSync('./ca-certificate.crt').toString(),
+        // rejectUnauthorized: false,
+        // ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString()
+        // key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+        // cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+      },
     }),
     CompanyModule,
   ],
